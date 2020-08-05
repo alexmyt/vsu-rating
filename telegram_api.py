2cc7fc324e52944465bc9699dde55164c14bdf1e
alexmyt <alexmyt1@yandex.ru>
2020-08-05 10:56:14 +0400

    Refactoring

--- telegram_api.py
+++ telegram_api.py
@@ -1,32 +1,47 @@
-from telegram.ext import Updater
-import os
-
-TOKEN = '1176844516:AAFzHPuP3kYv6VGbJI6mt7GTnMXgj-Qknwo'
-PORT = int(os.environ.get('PORT', 5000))
-
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-
 import logging
+from telegram.ext import Updater, CommandHandler
+from os import environ
+import vgu_ratings
+
+TOKEN = str(environ.get('TELEGRAM_BOT_TOKEN'))
+PORT = int(environ.get('PORT', 5000))
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please send /get command if you want see you rating.")
+  """Simple /start command"""
+  context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please send /get command if you want see you rating.")
 
-from telegram.ext import CommandHandler
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-import vgu_ratings
 def get(update, context):
+  """Get ratings from VSU site"""
   context.bot.send_message(chat_id=update.effective_chat.id, text=vgu_ratings.get_rating_doc())
 
-dispatcher.add_handler(CommandHandler('get', get))
+def main():
+  updater = Updater(token=TOKEN, use_context=True)
+  dispatcher = updater.dispatcher
 
-updater.start_webhook(listen="0.0.0.0",
-                      port=int(PORT),
-                      url_path=TOKEN)
-updater.bot.setWebhook('https://vsu-ratings.herokuapp.com/' + TOKEN)
+  # Bot commands
+  dispatcher.add_handler(CommandHandler('start', start))
+  dispatcher.add_handler(CommandHandler('get', get))
 
-updater.idle()
+  # Error handling
+  dispatcher.add_error_handler(error)
+
+  # We need use webhok instead start_pooling() because Heroku crash app
+  # if we not listen in defined port
+  updater.start_webhook(listen="0.0.0.0",
+                        port=int(PORT),
+                        url_path=TOKEN)
+  updater.bot.setWebhook('https://vsu-ratings.herokuapp.com/' + TOKEN)
+
+  updater.idle()
+
+
+if __name__ == '__main__':
+    main()
